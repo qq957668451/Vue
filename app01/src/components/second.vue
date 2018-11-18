3d35c44e8ddd4576da8ec9dfb84716e6f98688d3
zch <957668451@qq.com>
2018-11-18 12:08:18 +0800

    'first'

---
+++ app01/src/components/second.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="second">
+		<!-- <span :bind:title='msg'>  -->
+			<!-- 鼠标悬停几秒钟查看此处动态绑定的提示信息 -->
+		<!-- </span> -->
+		{{msg}}
+	</div>
+</template>
+<script>
+export default {
+  name: 'second',
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  }
+}
+</script>
